2411201d0d9a1e826017526394b4c988061997c0
Julaiti Alafate <jalafate@gmail.com>
2018-12-13 16:37:03 -0800

    updates

--- setup/setup-r5d.sh
+++ setup/setup-r5d.sh
@@ -10,7 +10,7 @@ sudo mount LABEL=MY_DISK /mnt
 sudo chown -R ubuntu /mnt
 
 sudo apt-get update
-sudo apt-get install -y awscli cargo
+sudo apt-get install -y awscli gcc
 echo "export EDITOR=vim" >> ~/.bashrc
 
 git config --global user.name "Julaiti Alafate"
@@ -20,13 +20,15 @@ git config --global push.default simple
 cd /mnt
 curl https://sh.rustup.rs -sSf > rustup.sh
 bash rustup.sh -y
-source $HOME/.cargo/env
 git clone $SPARROW_REPO sparrow
-git checkout $SPARROW_BRANCH
 git clone $TMSN_REPO tmsn
+cd tmsn
 git checkout $TMSN_BRANCH
-cd sparrow
+cd /mnt/sparrow
+git checkout $SPARROW_BRANCH
+source ~/.cargo/env
 cargo build --release > /dev/null 2> /dev/null &
+cd /mnt
 if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
     echo "Data files are not downloaded because AWS credentials are not set."
 else
