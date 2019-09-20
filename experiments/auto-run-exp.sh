a930fc5907e16c4986b40bd761fb99c5f0449fd3
Julaiti Alafate <jalafate@gmail.com>
2019-09-20 11:01:35 -0700

    fixed the issues with auto-run-exp

--- experiments/auto-run-exp.sh
+++ experiments/auto-run-exp.sh
@@ -1,3 +1,7 @@
+if [ "$#" -ne 1 ]; then
+    echo "Wrong paramters. Usage: ./auto-run-exp.sh <identity_file>"
+    exit
+fi
 
 mapfile -t servers < servers.txt
 
@@ -5,8 +9,21 @@ sampler=${servers[0]}
 scanners=("${servers[@]:1}")
 SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i $1 ubuntu@"
 
+echo "Set up config splice"
+$SSH_COMMAND$sampler \
+    "sed -i 's/num_iterations: 10/num_iterations: 1000/' /mnt/sparrow/examples/config_splice.yaml" &
+iter=0
+for addr in "${scanners[@]}"
+do
+    iter=$(expr $iter + 1)
+    $SSH_COMMAND$addr \
+        "sed -i 's/num_iterations: 10/num_iterations: 1000/' /mnt/sparrow/examples/config_splice.yaml" &
+done
+
+wait
+
 echo "Start training"
-RUN_COMMAND="/mnt/sparrow-scripts/experiments/run-exp.sh /mnt/sparrow/examples/config_splice.yaml y"
+RUN_COMMAND="cd /mnt; ./sparrow-scripts/experiments/run-exp.sh ./sparrow/examples/config_splice.yaml y"
 $SSH_COMMAND$sampler "$RUN_COMMAND" &
 for addr in "${scanners[@]}"
 do
