7e721706deb538cb38ada815b08d4bbe57e6e669
Julaiti Alafate <jalafate@gmail.com>
2019-09-26 00:10:05 -0700

    moved config setup from auto-setup to auto-run-exp

--- experiments/auto-run-exp.sh
+++ experiments/auto-run-exp.sh
@@ -7,23 +7,41 @@ mapfile -t servers < servers.txt
 
 sampler=${servers[0]}
 scanners=("${servers[@]:1}")
+scanners_list=""
+for addr in "${scanners[@]}"
+do
+    scanners_list=$scanners_list", \""$addr"\""
+done
+scanners_list="["$(echo $scanners_list | sed -e "s/, //")"]"
+
 SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i $1 ubuntu@"
+SED_COMMAND="
+    cd /mnt/sparrow; git reset --hard; \
+    sed -i 's/num_iterations: 10/num_iterations: 100/' /mnt/sparrow/examples/config_splice.yaml; \
+    sed -i 's/num_trees: 20/num_trees: 20/' /mnt/sparrow/examples/config_splice.yaml; \
+    sed -i 's/max_depth: 2/max_depth: 2/' /mnt/sparrow/examples/config_splice.yaml; \
+    sed -i 's/five-scanner/twenty-scanner/' /mnt/sparrow/examples/config_splice.yaml; \
+    sed -i 's/resume-training: false/resume-training: false/' /mnt/sparrow/examples/config_splice.yaml;"
 
 echo "Set up config splice"
-$SSH_COMMAND$sampler \
-    "sed -i 's/num_iterations: 10/num_iterations: 1000/' /mnt/sparrow/examples/config_splice.yaml" &
+$SSH_COMMAND$sampler "$SED_COMMAND \
+     sed -i 's/network: \[\]/network: $scanners_list/' /mnt/sparrow/examples/config_splice.yaml; \
+     sed -i 's/local_worker/sampler/' /mnt/sparrow/examples/config_splice.yaml; \
+     sed -i 's/: sampler/: sampler/' /mnt/sparrow/examples/config_splice.yaml;" &
 iter=0
 for addr in "${scanners[@]}"
 do
     iter=$(expr $iter + 1)
-    $SSH_COMMAND$addr \
-        "sed -i 's/num_iterations: 10/num_iterations: 1000/' /mnt/sparrow/examples/config_splice.yaml" &
+    $SSH_COMMAND$addr "$SED_COMMAND \
+        sed -i 's/network: \[\]/network: \[\]/' /mnt/sparrow/examples/config_splice.yaml; \
+        sed -i 's/local_worker/local_worker_$iter/' /mnt/sparrow/examples/config_splice.yaml;
+        sed -i 's/: sampler/: scanner/' /mnt/sparrow/examples/config_splice.yaml;" &
 done
 
 wait
 
 echo "Start training"
-RUN_COMMAND="cd /mnt; ./sparrow-scripts/experiments/run-exp.sh ./sparrow/examples/config_splice.yaml y"
+RUN_COMMAND="killall -9 sparrow; cd /mnt; ./sparrow-scripts/experiments/run-exp.sh ./sparrow/examples/config_splice.yaml y"
 $SSH_COMMAND$sampler "$RUN_COMMAND" &
 for addr in "${scanners[@]}"
 do
--- setup/auto-setup.sh
+++ setup/auto-setup.sh
@@ -6,6 +6,7 @@ fi
 
 SETUP_DIR="/home/ubuntu/sparrow-scripts/setup"
 SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i $1 ubuntu@"
+GIT_COMMAND="git clone https://github.com/arapat/sparrow-scripts.git"
 mapfile -t servers < servers.txt
 
 sampler=${servers[0]}
@@ -18,34 +19,14 @@ done
 scanners_list="["$(echo $scanners_list | sed -e "s/, //")"]"
 
 echo "Setup" $sampler
-$SSH_COMMAND$sampler "$SETUP_DIR/setup.sh $2 $3 $4 > /dev/null 2> /dev/null" &
+$SSH_COMMAND$sampler "$GIT_COMMAND; $SETUP_DIR/setup.sh $2 $3 $4 > /dev/null 2> /dev/null" &
 for addr in "${scanners[@]}"
 do
     echo "Setup the scanner" $addr
-    $SSH_COMMAND$addr "$SETUP_DIR/setup.sh nodata $3 $4 > /dev/null 2> /dev/null" &
+    $SSH_COMMAND$addr "$GIT_COMMAND; $SETUP_DIR/setup.sh nodata $3 $4 > /dev/null 2> /dev/null" &
 done
 
 echo "Waiting for the setup scripts to be finished"
 wait
 
-echo "Set up config splice"
-$SSH_COMMAND$sampler \
-    "sed -i 's/network: \[\]/network: $scanners_list/' /mnt/sparrow/examples/config_splice.yaml; \
-     sed -i 's/local_worker/sampler/' /mnt/sparrow/examples/config_splice.yaml;
-     sed -i 's/: sampler/: sampler/' /mnt/sparrow/examples/config_splice.yaml;
-     sed -i 's/five-scanner/twenty-scanner/' /mnt/sparrow/examples/config_splice.yaml;
-     sed -i 's/resume-training: false/resume-training: false/' /mnt/sparrow/examples/config_splice.yaml" &
-iter=0
-for addr in "${scanners[@]}"
-do
-    iter=$(expr $iter + 1)
-    $SSH_COMMAND$addr \
-        "sed -i 's/network: \[\]/network: \[\]/' /mnt/sparrow/examples/config_splice.yaml; \
-        sed -i 's/local_worker/local_worker_$iter/' /mnt/sparrow/examples/config_splice.yaml;
-        sed -i 's/: sampler/: scanner/' /mnt/sparrow/examples/config_splice.yaml;
-        sed -i 's/five-scanner/twenty-scanner/' /mnt/sparrow/examples/config_splice.yaml;
-        sed -i 's/resume-training: false/resume-training: false/' /mnt/sparrow/examples/config_splice.yaml" &
-done
-
-wait
 echo "Done."
