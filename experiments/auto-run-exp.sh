c9944aa30284fedcb69e70b997502b8cde1a92b9
Julaiti Alafate <jalafate@gmail.com>
2019-10-07 09:59:00 -0700

    pull latest sparrow before running exp

--- experiments/auto-run-exp.sh
+++ experiments/auto-run-exp.sh
@@ -18,7 +18,7 @@ scanners_list="["$(echo $scanners_list | sed -e "s/, //")"]"
 SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i $1 ubuntu@"
 SED_COMMAND="
     cd /mnt/sparrow-scripts; git reset --hard; git pull; \
-    cd /mnt/sparrow; git reset --hard; \
+    cd /mnt/sparrow; git reset --hard; git pull; \
     sed -i 's/num_iterations: 10$/num_iterations: 3000/' /mnt/sparrow/examples/config_splice.yaml; \
     sed -i 's/num_trees: 20$/num_trees: 500/' /mnt/sparrow/examples/config_splice.yaml; \
     sed -i 's/max_depth: 2$/max_depth: 2/' /mnt/sparrow/examples/config_splice.yaml; \
